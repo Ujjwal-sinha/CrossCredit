5ab8b4a8aa24655a5724df1f192b2e1d2a7f1839
Ujjwal Sinha <ujjwalsinha418@gmail.com>
2025-06-19 00:29:11 +0530

    all contracts pass test

--- contracts/mocks/MockRouter.sol
+++ contracts/mocks/MockRouter.sol
@@ -71,33 +71,6 @@ contract MockFunctionsRouter {
 
 
 
-contract MockERC20 {
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-    
-    constructor() {
-        balanceOf[msg.sender] = type(uint256).max;
-    }
-    
-    function transfer(address to, uint256 amount) external returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
-        return true;
-    }
-    
-    function approve(address spender, uint256 amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        return true;
-    }
-    
-    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-        require(allowance[from][msg.sender] >= amount, "Insufficient allowance");
-        allowance[from][msg.sender] -= amount;
-        balanceOf[from] -= amount;
-        balanceOf[to] += amount;
-        return true;
-    }
-}
 
 contract MockMainRouter {
     mapping(address => uint256) public creditScores;
