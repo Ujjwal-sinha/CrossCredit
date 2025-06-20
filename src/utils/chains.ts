781c04eb180defc8b9ad0817ae610a57aefdeca9
Arya kesarwani <aryakesharwani1234@gmail.com>
2025-06-21 02:32:05 +0530

    editted chains

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -4,21 +4,6 @@ export const CHAIN_INFO = {
     explorer: 'https://etherscan.io',
     currency: 'ETH'
   },
-  '0x3': {
-    name: 'Ropsten',
-    explorer: 'https://ropsten.etherscan.io',
-    currency: 'ETH'
-  },
-  '0x4': {
-    name: 'Rinkeby',
-    explorer: 'https://rinkeby.etherscan.io',
-    currency: 'ETH'
-  },
-  '0x5': {
-    name: 'Goerli',
-    explorer: 'https://goerli.etherscan.io',
-    currency: 'ETH'
-  },
   '0x89': {
     name: 'Polygon',
     explorer: 'https://polygonscan.com',
@@ -28,6 +13,30 @@ export const CHAIN_INFO = {
     name: 'Mumbai',
     explorer: 'https://mumbai.polygonscan.com',
     currency: 'MATIC'
+  },
+  // testnets sepolia
+  '0x1115': {
+    name: 'Sepolia',
+    explorer: 'https://sepolia.etherscan.io',
+    currency: 'ETH'
+  },
+  // tesnets Arbitrum
+  '0xa4b1': {
+    name: 'Arbitrum',
+    explorer: 'https://arbiscan.io',
+    currency: 'ETH'
+  },
+  // tesnets Avalanche
+  '0x82aF': {
+    name: 'Avalanche',
+    explorer: 'https://snowtrace.io',
+    currency: 'AVAX'
+  },
+  // tesnets Optimism
+  '0x4e454147': {
+    name: 'Optimism',
+    explorer: 'https://optimistic.etherscan.io',
+    currency: 'ETH'
   }
 } as const;
 
